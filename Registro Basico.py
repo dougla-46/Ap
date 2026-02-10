7e6c5c68792269af9b9246349cf91bda01c598a3
Douglas Trajano <douglastrajano45@gmail.com>
2026-02-10 18:35:31 -0300

    Add files via upload

---
+++ Registro Basico.py
@@ -0,0 +1,7 @@
+print('Qual é o dia que sua existecia miseravel começou?')
+dia = input('Dia')
+mes = input('Mês')
+ano = input('Ano')
+print('Sua Existencia nada agradavel começou no Dia', dia, 'do Mês', mes, 'do ano', ano,'?')
+
+
